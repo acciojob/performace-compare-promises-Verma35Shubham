c0e253feb07bb45003ecddc627d44806eb1be8f9
Shubham Verma <verma.shubham2537@gmail.com>
2023-06-12 11:16:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Array of API URLs to fetch data from
-const apiUrls = [
+const urls = [
   "https://jsonplaceholder.typicode.com/todos/1",
   "https://jsonplaceholder.typicode.com/todos/2",
   "https://jsonplaceholder.typicode.com/todos/3",
@@ -13,3 +13,40 @@ const apiUrls = [
 ];
 
 // You can write your code here
+
+const outputAll = document.getElementById('output-all');
+const outputAny = document.getElementById('output-any');
+
+const promises = urls.map(url => fetch(url));
+
+Promise.all(promises)
+  .then(responses => {
+    responses.forEach(response => {
+      const row = document.createElement('tr');
+      const urlCell = document.createElement('td');
+      const statusCell = document.createElement('td');
+      urlCell.textContent = response.url;
+      statusCell.textContent = response.status;
+      row.appendChild(urlCell);
+      row.appendChild(statusCell);
+      outputAll.appendChild(row);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+Promise.any(promises)
+  .then(response => {
+    const row = document.createElement('tr');
+    const urlCell = document.createElement('td');
+    const statusCell = document.createElement('td');
+    urlCell.textContent = response.url;
+    statusCell.textContent = response.status;
+    row.appendChild(urlCell);
+    row.appendChild(statusCell);
+    outputAny.appendChild(row);
+  })
+  .catch(error => {
+    console.log(error);
+  });
